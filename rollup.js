ffea8eafbc412fabb62a123db90291e3e7892363
Cal Henderson <cal@iamcal.com>
2010-08-31 17:38:34 -0500

    moved the collectpr into it's own file

--- rollup.js
+++ rollup.js
@@ -1,5 +1,6 @@
 var dgram = require('dgram');
 var sys = require('sys');
+var Collector = require('./lib/collector').Collector;
 
 var config = {
 	port		: 8464,
@@ -7,68 +8,6 @@ var config = {
 	check_ms	: 1000,
 };
 
-function Collector(){
-
-	var self = this;
-
-	this.buckets = {};
-
-	this.addData = function(stat, value){
-		var bucket = self.getBucket();
-		if (!self.buckets[bucket]) self.buckets[bucket] = {};
-		if (!self.buckets[bucket][stat]) self.buckets[bucket][stat] = [];
-		self.buckets[bucket][stat].push(value);
-	};
-
-	this.getBucket = function(){
-		return Math.round(new Date().getTime() / config.bucket_ms);
-	};
-
-	this.bucketToTime = function(bucket){
-		return bucket * config.bucket_ms;
-	};
-
-	this.rollupBuckets = function(){
-
-		var current = self.getBucket();
-
-		// get all current buckets in order
-		var buckets = [];
-		for (var i in self.buckets) buckets.push(i);
-		buckets = buckets.sort();
-
-		for (var i=0; i<buckets.length; i++){
-			var bucket = buckets[i];
-
-			if (bucket < current){
-
-				for (var j in self.buckets[bucket]){
-
-					var data = self.rollupData(self.buckets[bucket][j], bucket, j);
-					self.emit('data', data);
-				}
-
-				delete self.buckets[bucket];
-			}
-		}
-	};
-
-	this.rollupData = function(vals, bucket, stat){
-
-		return {
-			stat: stat,
-			ts: self.bucketToTime(bucket),
-			num: vals.length,
-			foo: 'bar',
-		};
-	};
-
-	setInterval(function(){
-		collector.rollupBuckets();
-	}, config.check_ms);
-};
-
-sys.inherits(Collector, process.EventEmitter);
 
 function num_keys(a){
 	var i=0;
@@ -76,7 +15,7 @@ function num_keys(a){
 	return i;
 }
 
-var collector = new Collector();
+var collector = new Collector(config);
 
 var server = dgram.createSocket('udp4', function(msg, rinfo){
 	var bits = msg.toString('utf8').split(':');
